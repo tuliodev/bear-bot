1eb4776e683d54761b1e1b2cd675fea3acbfcbf4
tuliodev <resendetulio51@gmail.com>
2021-03-21 12:02:53 -0300

    feat: Add a command handler

---
+++ src/commands/profile.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'profile',
+  description: 'Profile command',
+  execute(message, args) {
+    message.channel.send('Test');
+  },
+};
--- src/index.js
+++ src/index.js
@@ -1,7 +1,43 @@
+// Discord
 const Discord = require('discord.js');
-
-const { config, prefix } = require('./config/config.json');
-
+// Libs
+const fs = require('fs');
+// Config
+const { token, prefix } = require('./config/config.json');
+// Client
 const client = new Discord.Client();
+// Commands
+client.commands = new Discord.Collection();
+
+const commandFiles = fs.readdirSync(`${__dirname}/commands`).filter((file) => file.endsWith('.js'));
+
+// Turning on the bot
+client.once('ready', () => {
+  console.log('Bear bot online!');
+});
+
+console.log(prefix);
+
+// Command handler
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+client.on('message', (message) => {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+  const args = message.content.slice(prefix.length).trim().split(' ');
+  const command = args.shift().toLowerCase();
+
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply('Teve um problema ao executar o comando!');
+  }
+});
 
 client.login(token);
