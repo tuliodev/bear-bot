b6d5380363a828bd6a9e5e747feb3d877fc01e70
tuliodev <resendetulio51@gmail.com>
2021-03-21 16:16:21 -0300

    feat: add xp system with levelup

--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ const Discord = require('discord.js');
 // Libs
 const fs = require('fs');
 // Config
-const { token, prefix } = require('./config/config.json');
+const { token, prefix, xpSystemConfig } = require('./config/config.json');
 // Client
 const client = new Discord.Client();
 // Mongoose
 const mongoose = require('mongoose');
+// Mongoose models
+const User = require('./database/Models/User');
 // Commands
 client.commands = new Discord.Collection();
 
@@ -36,8 +38,56 @@ for (const file of commandFiles) {
   client.commands.set(command.name, command);
 }
 
-client.on('message', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', async (message) => {
+  if (!message.content.startsWith(prefix)) {
+    const userData = {
+      discord_id: message.author.id,
+      name: message.author.username,
+      coins: 0,
+      xp: 0,
+      level: 0,
+    };
+
+    const findUserById = await User.findOne({ discord_id: userData.discord_id });
+
+    if (!findUserById) {
+      if (!message.author.bot) {
+        const user = await User.create(userData);
+        return;
+      }
+      return;
+    }
+
+    // Level up system
+    let newXp = findUserById.xp;
+    let newLevel = findUserById.level;
+
+    await User.findOneAndUpdate({ discord_id: findUserById.discord_id }, { xp: newXp += xpSystemConfig.xpPerMessage });
+
+    // Level 1
+    if (newXp === 100) {
+      await User.findOneAndUpdate({ discord_id: findUserById.discord_id }, { level: newLevel += 1 });
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Bear Bot - Sistema de nível')
+        .setColor(0xff0000)
+        .setThumbnail('https://media.discordapp.net/attachments/441025017241665556/823272441190416384/bearbot2.png')
+        .setDescription(`Parabéns ${message.member.user} Você acabou de subir de nível, nível atual: 1`);
+
+      return message.channel.send(embed);
+    }
+    // Level 2
+    if (newXp === 200) {
+      await User.findOneAndUpdate({ discord_id: findUserById.discord_id }, { level: newLevel += 1 });
+      const embed = new Discord.MessageEmbed()
+        .setTitle('Bear Bot - Sistema de nível')
+        .setColor(0xff0000)
+        .setThumbnail('https://media.discordapp.net/attachments/441025017241665556/823272441190416384/bearbot2.png')
+        .setDescription(`Parabéns ${message.member.user} Você acabou de subir de nível, nível atual: 2`);
+
+      return message.channel.send(embed);
+    }
+  }
+
   const args = message.content.slice(prefix.length).trim().split(' ');
   const command = args.shift().toLowerCase();
 
