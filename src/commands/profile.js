27c4e6595197076f3a8e1042b3424adc766e5b8f
tuliodev <resendetulio51@gmail.com>
2021-03-21 14:10:58 -0300

    feat: add $profile v1.0.0 command

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,7 +1,18 @@
+const { MessageEmbed } = require('discord.js');
+
 module.exports = {
   name: 'profile',
   description: 'Profile command',
   execute(message, args) {
-    message.channel.send('Test');
+    const member = message.mentions.users.first() || message.author;
+    const avatar = member.displayAvatarURL();
+
+    const embed = new MessageEmbed()
+      .setTitle(member.username)
+      .setColor(0xff0000)
+      .setDescription('Avatar do usuário:')
+      .setImage(avatar);
+
+    return message.channel.send(embed);
   },
 };
