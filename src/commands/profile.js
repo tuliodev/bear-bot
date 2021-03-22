ee7ac6ebb163ce01ac09c5bca8a4ec7fef08a5b9
tuliodev <resendetulio51@gmail.com>
2021-03-22 08:17:10 -0300

    refactor: Added on profile command xp / level info

--- src/commands/profile.js
+++ src/commands/profile.js
@@ -1,16 +1,34 @@
 const { MessageEmbed } = require('discord.js');
+// embed
+
+// Mongoose models
+const User = require('../database/Models/User');
 
 module.exports = {
   name: 'profile',
   description: 'Profile command',
-  execute(message, args) {
+  async execute(message, args) {
     const member = message.mentions.users.first() || message.author;
     const avatar = member.displayAvatarURL();
 
+    const userDiscordId = member.id;
+
+    const findUserById = await User.findOne({ discord_id: userDiscordId });
+
+    if (!findUserById) {
+      const embed = new MessageEmbed()
+        .setTitle('Bear bot - Sistema de nível')
+        .setColor(0xff0000)
+        .setDescription('Nenhum usuário encontrado')
+        .setThumbnail('https://media.discordapp.net/attachments/441025017241665556/823272441190416384/bearbot2.png');
+
+      return message.channel.send(embed);
+    }
+
     const embed = new MessageEmbed()
-      .setTitle(member.username)
+      .setTitle(':bear:  Informações do perfil')
       .setColor(0xff0000)
-      .setDescription('Avatar do usuário:')
+      .setDescription(`**- Nome** ${member.username} \n **- Nível:** ${findUserById.level}\n **- Xp:** ${findUserById.xp} \n** - Avatar:**`)
       .setImage(avatar);
 
     return message.channel.send(embed);
